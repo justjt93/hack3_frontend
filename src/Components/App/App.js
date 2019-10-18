fbbc86d24a940ecd193f56a995acb7497c45c97f
JT <jakubtalacko@gmail.com>
2019-10-18 11:23:14 +0200

    theaters component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Page from '../Page';
-
+import Page from "../Page";
 
 export default class App extends React.Component {
-
-    render() {
-        return (
-            <Page />
-        );
-    }
+  render() {
+    return <Page />;
+  }
 }
--- src/Components/Theaters/Theaters.jsx
+++ src/Components/Theaters/Theaters.jsx
@@ -1,31 +1,21 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Theaters(props) {
-    return (
 
-        <>
-            <h1>Welcome to Daydream Cinemas</h1>
+  return (
+    <>
+      <h1>Welcome to Daydream Cinemas</h1>
 
-            <div className="calltoaction">Please select a theater near you</div>
+      <div className='calltoaction'>Please select a theater near you</div>
 
-            <ul>
-                <li>
-                    <Link to="/prague">Prague</Link>
-                </li>
-                <li>
-                    <Link to="/london">London</Link>
-                </li>
-                <li>
-                    <Link to="/brussels">Brussels</Link>
-                </li>
-            </ul>
-        </>
-
-    )
+      <ul>
+        {props.data.map(item => (
+          <li>
+            <Link to={`/${item.slug}`}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
